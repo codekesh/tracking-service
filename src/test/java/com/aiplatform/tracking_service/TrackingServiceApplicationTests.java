8d0ab9eca92e93e2172464b799714ded8ea7788b
codekesh <201117@iiitt.ac.in>
2026-01-17 18:50:21 +0530

    intial commit

---
+++ src/test/java/com/aiplatform/tracking_service/TrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiplatform.tracking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
